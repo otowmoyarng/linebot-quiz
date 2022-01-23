ec6b0c6d559c80c01210ca43cf50205814d0d4f1
otowmoyarng <hidemusiam@gmail.com>
2022-01-23 17:11:13 +0900

    try-catch追加

--- src/Dispatcher.js
+++ src/Dispatcher.js
@@ -4,22 +4,26 @@
  */
 function doPost(request) {
 
-    // POSTリクエストをJSONデータにパース
-    const receiveJSON = JSON.parse(request.postData.contents);
-    const event = receiveJSON.events[0];
-    console.log(`event.type:${event.type}, userId:${event.source.userId}, test:${event.message.text}, replyToken:${event.replyToken}`);
+    try {
+        // POSTリクエストをJSONデータにパース
+        const receiveJSON = JSON.parse(request.postData.contents);
+        const event = receiveJSON.events[0];
+        console.log(`event.type:${event.type}, userId:${event.source.userId}, test:${event.message.text}, replyToken:${event.replyToken}`);
 
-    // アクセスユーザーを登録
-    if (event.type == 'follow') {
-        return follow(event.source.userId);
-    }
+        // アクセスユーザーを登録
+        if (event.type == 'follow') {
+            return follow(event.source.userId);
+        }
 
-    // ブロックされたとき、該当ユーザーを削除
-    if (event.type == 'unfollow') {
-        return unfollow(event.source.userId);
-    }
+        // ブロックされたとき、該当ユーザーを削除
+        if (event.type == 'unfollow') {
+            return unfollow(event.source.userId);
+        }
 
-    if (event.message.text === Operation.Start) {
+        if (event.message.text === Operation.Start) {
 
+        }
+    } catch (ex) {
+        console.error("エラー発生", ex);
     }
 }
