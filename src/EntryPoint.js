5c6b13d4bf07ac3e91e70fbbaa596e66e0b0a5a8
otowmoyarng <hidemusiam@gmail.com>
2022-01-29 13:10:17 +0900

    ファイル名を訂正

---
+++ src/EntryPoint.js
@@ -0,0 +1,54 @@
+/**
+ * LINEからのwebhookに対応するエントリポイント
+ * @param request HTTPRequest
+ */
+function doPost(request) {
+
+    try {
+        // POSTリクエストをJSONデータにパース
+        const receiveJSON = JSON.parse(request.postData.contents);
+        const event = receiveJSON.events[0];
+
+        routing(event);
+
+    } catch (ex) {
+        console.error("エラー発生", ex);
+    }
+}
+
+function routing(event) {
+    Logger.WriteLog(`event.type:${event.type}, userId:${event.source.userId}, test:${event.message.text}, replyToken:${event.replyToken}`);
+
+    // アクセスユーザーを登録
+    if (event.type == 'follow') {
+        return follow(event.source.userId);
+    }
+
+    // ブロックされたとき、該当ユーザーを削除
+    if (event.type == 'unfollow') {
+        return unfollow(event.source.userId);
+    }
+
+    let status = sheetAccessor.getStatus();
+
+    // クイズ中
+    if (status === State.Answering) {
+        return quiz.Question(event.replyToken);
+        // 第１問を出す
+    } else if (event.message.text === Operation.Start) {
+        return quiz.Start(event.replyToken);
+    } else {
+        // TODO
+        const replyMessages = [
+            `event.type:${event.type}`,
+            `userId:${event.source.userId}`,
+            `text:${event.message.text}`,
+            `replyToken:${event.replyToken}`,
+        ];
+        replyMessages.forEach((message) => {
+            Logger.WriteLog(message);
+        });
+        return LineApiDriver.replyToUser(event.replyToken, replyMessages);
+        // TODO
+    }
+}
